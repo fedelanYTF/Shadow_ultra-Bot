313466fbf162542afcffae4fa3e9ceec60a0baaf
fedelanYTF <fedelanytF@gmail.com>
2025-05-09 12:14:21 -0300

    Update info-creador.js

--- plugins/info-creador.js
+++ plugins/info-creador.js
@@ -11,8 +11,8 @@ let handler = async (m, { conn, usedPrefix, text, args, command }) => {
     // VCARD
     let list = [{
         displayName: "𝘧𝘦𝘥𝘦𝘭𝘢𝘯𝘠𝘛",
-        vcard: `BEGIN:VCARD\nVERSION:3.0\nFN: Cristian Escobar
-\nitem1.TEL;waid=5491164352241:5491164352241\nitem1.X-ABLabel:Número\nitem2.EMAIL;type=INTERNET: fedelanytf@gmail.com\nitem2.X-ABLabel:Email\nitem3.URL:https://www.instagram.com/shadowbot.md\nitem3.X-ABLabel:Internet\nitem4.ADR:;; Perú;;;;\nitem4.X-ABLabel:Region\nEND:VCARD`,
+        vcard: `BEGIN:VCARD\nVERSION:3.0\nFN: 𝘧𝘦𝘥𝘦𝘭𝘢𝘯𝘠𝘛
+\nitem1.TEL;waid=5491156178758:5491156178758\nitem1.X-ABLabel:Número\nitem2.EMAIL;type=INTERNET: fedelanytf@gmail.com\nitem2.X-ABLabel:Email\nitem3.URL:https://www.instagram.com/shadowbot.md\nitem3.X-ABLabel:Internet\nitem4.ADR:;; Perú;;;;\nitem4.X-ABLabel:Region\nEND:VCARD`,
     }];
 
     await conn.sendMessage(m.chat, {
@@ -26,7 +26,7 @@ let handler = async (m, { conn, usedPrefix, text, args, command }) => {
                 title: 'һ᥆ᥣᥲ s᥆ᥡ ᥱᥣ ᥴrᥱᥲძ᥆r ძᥱ sһᥲძ᥆ᥕ ᑲ᥆𝗍',
                 body: dev,
                 thumbnailUrl: 'https://files.catbox.moe/8an9fs.jpg',
-                sourceUrl: 'https://wa.me/5491164352241?text=Vengo+Del+Comando+.owner',
+                sourceUrl: 'https://wa.me/5491156178758?text=Vengo+Del+Comando+.owner',
                 mediaType: 1,
                 renderLargerThumbnail: true
             }
